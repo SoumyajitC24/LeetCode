c79800bb53fe5d568d8e260d344cbc854e24b5cf
SoumyajitC24 <84794901+SoumyajitC24@users.noreply.github.com>
2024-07-17 09:34:55 +0530

    Update containsDuplicate2.py

--- containsDuplicate2.py
+++ containsDuplicate2.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 Example 1:
@@ -20,15 +20,13 @@ Constraints:
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109
 0 <= k <= 105
-*/
 
-/*
 Solution: Dictionary/HashTable
 1. Enumerate the list to get index+numbers
 2. If nummber not in Dictionary, add it as (key,value) = (num,index)
 3. If number is present, check if difference with current_index is <=k, return True
 4. Else, dict(num) = current index
-*/
+'''
 
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
